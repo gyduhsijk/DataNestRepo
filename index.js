b0d87c5b5be72e9fead30e170181a738f02524be
gyduhsijk <isjcrispenril16@hotmail.com>
2024-05-11 13:30:59 +0700

    demo project commit

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const bubbleSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
-    }
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
   }
-  return arr;
-};
+}
